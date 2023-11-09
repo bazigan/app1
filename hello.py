881e3345e7c4bd422a42fe52d52595bf03a39557
bazigan <bazigannetwork@gmail.com>
2023-11-09 15:48:32 +0700

    update

--- hello.py
+++ hello.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, Coba dulu!'
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
